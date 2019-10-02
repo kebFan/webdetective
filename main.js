1de35f0b521d265da3c99d9308bee5d1471813bc
keb <keburefantaye@gmail.com>
2019-10-02 11:49:29 -0400

    update javascript and project division tasks

---
+++ main.js
@@ -0,0 +1,24 @@
+// steps
+
+int main(){
+
+}
+
+// connect html and js
+// connect to the internet
+  // make js understand html button click
+//  when scan button is clicked
+  // show if the inpt is not valid website
+  // get website url as string fron the input tab
+  // check if the stirng is a website using REG EXP
+      // http_or_https_checker() method
+  // once we get the string url check for http and https
+    // add the result to the report table
+  // show loading buffer while the report is prossesed
+  // check each api
+      // google security check
+      // sql api
+      // ssl api
+      // more and
+// show the report in the page
+// downlodable report function
